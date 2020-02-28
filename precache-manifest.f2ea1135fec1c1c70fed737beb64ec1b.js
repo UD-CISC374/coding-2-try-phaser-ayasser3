a0a7fd6b08d92cc1ae9c676a9215f0dccaf36f82
Yasser Abdelaal <ayasser@udel.edu>
2020-02-28 15:27:13 -0500

    Updates

---
+++ precache-manifest.f2ea1135fec1c1c70fed737beb64ec1b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f8e8c6e4556ede29249d9b99bf67a8",
+    "url": "assets/8bitsunset.jpg"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "assets/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "assets/beam.ogg"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "assets/beam.png"
+  },
+  {
+    "revision": "6ef48a18244c2975345b13cdb7faca2b",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "b1d8ef8027eff2fbf8daf08e66909269",
+    "url": "assets/clouds.png"
+  },
+  {
+    "revision": "d056a09f3a7388539be2822874b4ea0d",
+    "url": "assets/diving.png"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "assets/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "assets/explosion.ogg"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "assets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "assets/font.xml"
+  },
+  {
+    "revision": "837b573aa49c94f0c7bfea58bc7657e8",
+    "url": "assets/gokussj3.png"
+  },
+  {
+    "revision": "e543e0f44f5220d17ccab1be15d518f0",
+    "url": "assets/parachute.png"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "assets/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "assets/pickup.ogg"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "assets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "assets/power-up.png"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "assets/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "assets/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "assets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "assets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "assets/ship3.png"
+  },
+  {
+    "revision": "68ca26f1cf015647ab1b576534fd6ec3",
+    "url": "index.html"
+  },
+  {
+    "revision": "0124601671c40a02227c",
+    "url": "main.f53eebd19152ceca223a.bundle.js"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "vendors.a047ee0740c7d21714d9.bundle.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/phaser-test-game/precache-manifest.7bd8aef4e053f3d0bcbe2320a580d723.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.f2ea1135fec1c1c70fed737beb64ec1b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
